f2fa84a7d6b53a676471b643d4f4ff6e26643845
Tarik TIRE <tarik.tire@fairefaire.fr>
2016-08-18 16:26:45 +0200

    dist directory for npm git install

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+require("materialize");
+// export {Materialize,MaterializeOptions} from "./materialize";
+var materialize_directive_1 = require("./materialize-directive");
+exports.MaterializeDirective = materialize_directive_1.MaterializeDirective;
+var materialize_module_1 = require("./materialize-module");
+exports.MaterializeModule = materialize_module_1.MaterializeModule;
+Waves.displayEffect();
+function toast() {
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i - 0] = arguments[_i];
+    }
+    Materialize.toast.apply(Materialize, args);
+}
+exports.toast = toast;
